4ec0fcb428495827e6a4b3c67d19c8baf2f3e5d1
choudhary2205rahul@gmai.com <choudhary2205rahul@gmai.com>
2021-08-04 16:17:30 +0530

    update

---
+++ src/test/java/com/crazzy/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazzy.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
